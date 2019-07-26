e7b60bffeb080fb3751fcb1b5d6b7ec3bb75b1cc
armjim14 <ajimenez1497@gmail.com>
2019-07-26 00:53:32 -0700

    different

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,9 +7,6 @@ function getPage(){
     this.ques = (res) => {
         return res.sendFile(path.join(__dirname, "../public/questions.html"));
     }
-    this.result = (res) => {
-        return res.sendFile(path.join(__dirname, "../public/result.html"));
-    }
 }
 
 module.exports = getPage;
